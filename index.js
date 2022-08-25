0b78cb6b22a44ddcbd151be2ce3a5a7d19ca0a9a
dmnitprof <dmn.itprof@gmail.com>
2022-08-25 10:22:49 +0300

    add post.login

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import { validationResult } from 'express-validator'
 
 import { registerValidation } from './validations/auth.js'
 
-import userModal from './models/User.js'
+import UserModel from './models/User.js'
+import checkAuth from './utils/checkAuth.js'
 
 mongoose
     .connect(
@@ -18,6 +19,45 @@ const app = express()
 
 
 app.use(express.json())
+
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email })
+
+        if (!user) {
+            return res.status(404).json({
+                message: 'Пользователь не найден'
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+
+        if (!isValidPass) {
+            return res.status(400).json({
+                message: 'Неверный логин или пароль'
+            })
+        }
+
+        const token = jwt.sign(
+            { _id: user._id },
+            'Valeria09',
+            { expiresIn: '30d' }
+        )
+
+        const { passwordHash, ...userData } = user._doc
+
+        res.json({
+            ...userData,
+            token
+        })
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось авторизоваться!'
+        })
+    }
+})
 
 app.post('/auth/register', registerValidation, async (req, res) => {
     try {
@@ -31,7 +71,7 @@ app.post('/auth/register', registerValidation, async (req, res) => {
         const sail = await bcrypt.genSalt(10)
         const hash = await bcrypt.hash(password, sail)
 
-        const doc = new userModal({
+        const doc = new UserModel({
             email: req.body.email,
             fullName: req.body.fullName,
             avatarUrl: req.body.avatarUrl,
@@ -41,9 +81,13 @@ app.post('/auth/register', registerValidation, async (req, res) => {
 
         const user = await doc.save()
 
-        const token = jwt.sign({ _id: user._id }, 'Valeria09', { expiresIn: '30d' })
+        const token = jwt.sign(
+            { _id: user._id },
+            'Valeria09',
+            { expiresIn: '30d' }
+        )
 
-        const { passwordHash, ...userData} = user._doc
+        const { passwordHash, ...userData } = user._doc
 
         res.json({
             ...userData,
@@ -60,6 +104,16 @@ app.post('/auth/register', registerValidation, async (req, res) => {
 /*
 mongodb+srv://<username>:<password>@cluster0.7mljn97.mongodb.net/?retryWrites=true&w=majority
 */
+
+app.get('/auth/me', checkAuth, (req, res) => {
+    try {
+        res.json({
+            success: true
+        })
+
+    } catch (err) {}
+})
+
 app.listen(4444, '192.168.1.111', (err) => {
     if (err) {
         console.log(err)
