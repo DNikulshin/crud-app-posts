584f9144bdc1cdee8c9075fa33d0e97e7b996f46
dmnitprof <dmn.itprof@gmail.com>
2022-08-26 22:01:53 +0300

    end setup

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
+import multer from 'multer'
 import mongoose from 'mongoose'
-import bcrypt from 'bcrypt'
-import { validationResult } from 'express-validator'
 
-import { registerValidation } from './validations/auth.js'
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js'
 
-import UserModel from './models/User.js'
-import checkAuth from './utils/checkAuth.js'
+import { UserController, PostController } from './controllers/index.js'
+import {checkAuth, handleValidationErrors} from './utils/index.js'
 
 mongoose
     .connect(
@@ -17,102 +15,42 @@ mongoose
 
 const app = express()
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage })
+
 
 app.use(express.json())
+app.use('/uploads', express.static('uploads'))
 
-app.post('/auth/login', async (req, res) => {
-    try {
-        const user = await UserModel.findOne({ email: req.body.email })
+app.post('/auth/login', loginValidation ,handleValidationErrors, UserController.login)
+app.post('/auth/register', registerValidation,  handleValidationErrors, UserController.register)
+app.get('/auth/me', checkAuth, UserController.getMe)
 
-        if (!user) {
-            return res.status(404).json({
-                message: 'Пользователь не найден'
-            })
-        }
-
-        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
-
-        if (!isValidPass) {
-            return res.status(400).json({
-                message: 'Неверный логин или пароль'
-            })
-        }
-
-        const token = jwt.sign(
-            { _id: user._id },
-            'Valeria09',
-            { expiresIn: '30d' }
-        )
-
-        const { passwordHash, ...userData } = user._doc
-
-        res.json({
-            ...userData,
-            token
-        })
-
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            message: 'Не удалось авторизоваться!'
-        })
-    }
+app.post('/upload', checkAuth , upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`
+    })
 })
 
-app.post('/auth/register', registerValidation, async (req, res) => {
-    try {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            return res.status(400).json(errors.array())
-        }
-
-        const password = req.body.password
-
-        const sail = await bcrypt.genSalt(10)
-        const hash = await bcrypt.hash(password, sail)
-
-        const doc = new UserModel({
-            email: req.body.email,
-            fullName: req.body.fullName,
-            avatarUrl: req.body.avatarUrl,
-            passwordHash: hash
-        })
-
-
-        const user = await doc.save()
-
-        const token = jwt.sign(
-            { _id: user._id },
-            'Valeria09',
-            { expiresIn: '30d' }
-        )
-
-        const { passwordHash, ...userData } = user._doc
-
-        res.json({
-            ...userData,
-            token
-        })
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            message: 'Не удалось зарегистрироваться!'
-        })
-    }
-
-})
-/*
-mongodb+srv://<username>:<password>@cluster0.7mljn97.mongodb.net/?retryWrites=true&w=majority
-*/
-
-app.get('/auth/me', checkAuth, (req, res) => {
-    try {
-        res.json({
-            success: true
-        })
-
-    } catch (err) {}
-})
+app.get('/posts', PostController.getAll)
+app.get('/posts/:id', PostController.getOne)
+app.post('/posts',  checkAuth ,postCreateValidation ,PostController.create)
+app.delete('/posts/:id', checkAuth , PostController.remove)
+app.patch(
+    '/posts/:id',
+    checkAuth ,
+    postCreateValidation,
+    handleValidationErrors,
+    PostController.update
+)
 
 app.listen(4444, '192.168.1.111', (err) => {
     if (err) {
